822c8fa2926d60200aa9f6074d5e139d9a50c640
Psilon <pzixel@gmail.com>
2021-05-24 18:11:40 +0300

    fix benchs

--- benches/my_bench.rs
+++ benches/my_bench.rs
@@ -7,6 +7,28 @@ const TOTAL_NUMS: usize = 100000;
 const SAMPLES: usize = 500;
 const SEARCHES_COUNT: usize = 10;
 
+fn binary_search(nums: &[i32], searches: &[i32]) -> i32 {
+    let mut counter = 0;
+    for i in searches.iter() {
+        if let Ok(_) = nums.binary_search(&i) {
+            counter += 1;
+        };
+    }
+    return counter;
+}
+
+fn linear_search(nums: &[i32], searches: &[i32]) -> i32 {
+    let mut counter = 0;
+    for i in searches.iter() {
+        for x in nums.iter() {
+            if x == i {
+                counter += 1;
+            };
+        }
+    }
+    return counter;
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("Array search");
     let arrs: Vec<_> = (0..SAMPLES).map(|_| {
@@ -15,48 +37,19 @@ fn criterion_benchmark(c: &mut Criterion) {
     }).collect();
     let mut rng = rand::thread_rng();
     for (i, nums) in arrs.iter().enumerate() {
-        let sorted_nums: Vec<_> = nums.iter().sorted().collect();
+        let sorted_nums: Vec<_> = nums.iter().sorted().cloned().collect();
         let searches: Vec<_> = (0..SEARCHES_COUNT).map(|_| nums[rng.gen_range(0..nums.len())]).collect();
 
         let nums = nums.clone().into_boxed_slice();
         let sorted_nums = sorted_nums.into_boxed_slice();
         let searches = searches.into_boxed_slice();
 
-        group.bench_with_input(BenchmarkId::new("Binary", i), &(&sorted_nums, &searches),
-                               |b, (nums, searches)| b.iter(|| {
-                                   let mut counter = 0;
-                                   for i in searches.iter() {
-                                       if let Ok(_) = nums.binary_search(&i) {
-                                           counter += 1;
-                                       };
-                                   }
-                                   return counter;
-                               }));
-        group.bench_with_input(BenchmarkId::new("Sorted", i), &(&sorted_nums, &searches),
-                               |b, (nums, searches)| b.iter(|| {
-                                   let mut counter = 0;
-                                   for i in searches.iter() {
-                                       for x in nums.iter() {
-                                           if *x == i {
-                                               counter += 1;
-                                           };
-                                       }
-                                   }
-                                   return counter;
-                               }));
-
-        group.bench_with_input(BenchmarkId::new("Unsorted", i), &(nums, &searches),
-                               |b, (nums, searches)| b.iter(|| {
-                                   let mut counter = 0;
-                                   for i in searches.iter() {
-                                       for x in nums.iter() {
-                                           if x == i {
-                                               counter += 1;
-                                           };
-                                       }
-                                   }
-                                   return counter;
-                               }));
+        group.bench_with_input(BenchmarkId::new("Binary", i), &(sorted_nums.clone(), searches.clone()),
+                               |b, (nums, searches)| b.iter(|| binary_search(&nums, searches.as_ref())));
+        group.bench_with_input(BenchmarkId::new("Sorted", i), &(sorted_nums.clone(), searches.clone()),
+                               |b, (nums, searches)| b.iter(|| linear_search(&nums, searches.as_ref())));
+        group.bench_with_input(BenchmarkId::new("Unsorted", i), &(nums.clone(), searches.clone()),
+                               |b, (nums, searches)| b.iter(|| linear_search(&nums, searches.as_ref())));
     }
     group.finish();
 }
